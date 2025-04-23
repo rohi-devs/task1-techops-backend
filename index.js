a8eefb18f28c1845dec0ebd30d92b612d8b57cc0
Visvess <amvisvesswar2004@gmail.com>
2025-04-23 08:11:22 +0530

    cors - vercel path added

--- index.js
+++ index.js
@@ -3,56 +3,62 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { PrismaClient } = require("@prisma/client");
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const cookieParser = require("cookie-parser");
 const prisma = new PrismaClient();
 const app = express();
 
 // app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task1-techops-frontend.vercel.app",
+  "https://task1-techops-frontend-nxx6ee0uy-vishs-projects-b03efeba.vercel.app",
+  "https://task1-techops-frontend-vishs-projects-b03efeba.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://task1-techops-frontend.vercel.app",
-      "https://task1-techops-frontend-nxx6ee0uy-vishs-projects-b03efeba.vercel.app",
-      "https://task1-techops-frontend-vishs-projects-b03efeba.vercel.app/"
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
-
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Admin API Documentation',
-      version: '1.0.0',
-      description: 'API documentation for the admin routes',
+      title: "Admin API Documentation",
+      version: "1.0.0",
+      description: "API documentation for the admin routes",
     },
     components: {
       securitySchemes: {
         BearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
   },
-  apis: ['./src/routes/*.js'], // path to your API routes
+  apis: ["./src/routes/*.js"], // path to your API routes
 };
 
 const specs = swaggerJsdoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
   console.log("Reached CTF complaint handler backend");
   res.send("Complaint handler route accessed");
 });
-
 
 app.use(express.json());
 app.use(morgan("dev"));
